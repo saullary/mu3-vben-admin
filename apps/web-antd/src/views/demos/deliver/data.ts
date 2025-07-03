b4185cc38ea9b139c283e4c6a056609aa0761128
HQs <409337645@qq.com>
2025-07-03 08:25:27 +0800

    deliver

---
+++ apps/web-antd/src/views/demos/deliver/data.ts
@@ -0,0 +1,41 @@
+import type { VxeTableGridOptions } from '@vben/plugins/vxe-table';
+import type { OnActionClickFn } from '#/adapter/vxe-table';
+
+export function useColumns(
+  onActionClick: OnActionClickFn<any>,
+): VxeTableGridOptions<any>['columns'] {
+  return [
+    {
+      field: 'company',
+      title: '快递公司', //$t('kd.company'),
+      minWidth: 100,
+    },
+    {
+      align: 'center',
+      field: 'order_no',
+      title: '订单号',
+      // width: 100,
+    },
+    {
+      field: 'phone',
+      title: '手机号',
+      // width: 200,
+    },
+    {
+      field: 'result',
+      slots: { default: 'result' },
+      title: '物流信息',
+      // width: 200,
+    },
+    {
+      field: 'status',
+      title: '状态',
+      // width: 200,
+    },
+    {
+      field: 'created_t',
+      title: '创建时间',
+      // width: 180,
+    },
+  ];
+}
---
+++ apps/web-antd/src/views/demos/deliver/list.vue
@@ -0,0 +1,96 @@
+<script lang="ts" setup>
+import type {
+  OnActionClickParams,
+  VxeTableGridOptions,
+} from '#/adapter/vxe-table';
+
+import { Page, useVbenDrawer } from '@vben/common-ui';
+import { $t } from '@vben/locales';
+
+// import { MenuBadge } from '@vben-core/menu-ui';
+
+import { Button, message } from 'ant-design-vue';
+
+import { useVbenVxeGrid } from '#/adapter/vxe-table';
+
+import { useColumns } from './data';
+import { queryAdmin, upsertAdmin } from '#/api';
+
+const tableName = 'deliver_log';
+
+const [Grid, gridApi] = useVbenVxeGrid({
+  gridOptions: {
+    columns: useColumns(onActionClick),
+    height: 'auto',
+    keepSource: true,
+    pagerConfig: {
+      enabled: false,
+    },
+    proxyConfig: {
+      ajax: {
+        query: queryAdmin(tableName, {
+          _tree: 1,
+        }),
+      },
+    },
+    rowConfig: {
+      keyField: 'id',
+    },
+    toolbarConfig: {
+      custom: true,
+      export: false,
+      refresh: { code: 'query' },
+      zoom: true,
+    },
+    treeConfig: {
+      parentField: 'pid',
+      rowField: 'id',
+      transform: false,
+    },
+  } as VxeTableGridOptions,
+});
+
+function onActionClick({ code, row }: OnActionClickParams<any>) {
+  switch (code) {
+    case 'delete': {
+      onDelete(row);
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+}
+
+function onRefresh() {
+  gridApi.query();
+}
+
+function onDelete(row: any) {
+  const hideLoading = message.loading({
+    content: $t('ui.actionMessage.deleting', [row.name]),
+    duration: 0,
+    key: 'action_process_msg',
+  });
+  upsertAdmin(tableName, null, row.id)
+    .then(() => {
+      message.success({
+        content: $t('ui.actionMessage.deleteSuccess', [row.name]),
+        key: 'action_process_msg',
+      });
+      onRefresh();
+    })
+    .catch(() => {
+      hideLoading();
+    });
+}
+</script>
+<template>
+  <Page auto-content-height>
+    <Grid>
+      <template #result="{ row }">
+        <div>res</div>
+      </template>
+    </Grid>
+  </Page>
+</template>
