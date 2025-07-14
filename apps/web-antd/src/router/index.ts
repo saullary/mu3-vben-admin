f717366893537374b4948d531632bbd4e06706ea
HQs <409337645@qq.com>
2025-07-14 11:30:07 +0800

    router

--- apps/web-antd/src/router/index.ts
+++ apps/web-antd/src/router/index.ts
@@ -13,10 +13,10 @@ import { routes } from './routes';
  *  @zh_CN 创建vue-router实例
  */
 const router = createRouter({
-  history:
-    import.meta.env.VITE_ROUTER_HISTORY === 'hash'
-      ? createWebHashHistory(import.meta.env.VITE_BASE)
-      : createWebHistory(import.meta.env.VITE_BASE),
+  // import.meta.env.VITE_ROUTER_HISTORY === 'hash'
+  //     ? createWebHashHistory(import.meta.env.VITE_BASE)
+  //     :
+  history: createWebHistory(import.meta.env.VITE_BASE),
   // 应该添加到路由的初始路由列表。
   routes,
   scrollBehavior: (to, _from, savedPosition) => {
