b91ac037885d0ea442aee66545f6e6c090802602
Junlei <749101340@qq.com>
2025-07-16 13:45:15 +0800

    refactor(路由配置): 门店管理->门店商品+订单物流
    - 原classify, 改为 goods_type

---
+++ apps/web-antd/src/router/routes/modules/shopAndGoods.ts
@@ -0,0 +1,42 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    meta: {
+      icon: 'ic:baseline-view-in-ar',
+      keepAlive: true,
+      order: 1000,
+      title: '门店商品',
+    },
+    name: 'shop',
+    path: '/shop',
+    children: [
+      {
+        meta: {
+          title: '门店管理',
+        },
+        name: 'Shop',
+        path: '/shop/shop',
+        component: () => import('#/views/shop/shop/list.vue'),
+      },
+      {
+        meta: {
+          title: '商品分类',
+        },
+        name: 'GoodsType',
+        path: '/shop/goodsType',
+        component: () => import('#/views/shop/goods_type/list.vue'),
+      },
+      {
+        meta: {
+          title: '商品管理',
+        },
+        name: 'Goods',
+        path: '/shop/goods',
+        component: () => import('#/views/shop/goods/list.vue'),
+      },
+    ],
+  },
+];
+
+export default routes;
--- apps/web-antd/src/router/routes/modules/trade.ts
+++ apps/web-antd/src/router/routes/modules/trade.ts
@@ -6,35 +6,11 @@ const routes: RouteRecordRaw[] = [
       icon: 'ic:baseline-view-in-ar',
       keepAlive: true,
       order: 1000,
-      title: '电商管理', // $t('demos.title'),
+      title: '订单物流',
     },
     name: 'Trade',
     path: '/trade',
     children: [
-      {
-        meta: {
-          title: '门店管理',
-        },
-        name: 'Shop',
-        path: '/trade/shop',
-        component: () => import('#/views/trade/shop/list.vue'),
-      },
-      {
-        meta: {
-          title: '商品分类',
-        },
-        name: 'Classify',
-        path: '/trade/cassify',
-        component: () => import('#/views/trade/classify/list.vue'),
-      },
-      {
-        meta: {
-          title: '商品管理',
-        },
-        name: 'Goods',
-        path: '/trade/goods',
-        component: () => import('#/views/trade/goods/list.vue'),
-      },
       {
         meta: {
           title: '快递记录',
