4720316ed8ecf8dc6af87f8ed4bb71a3bfe04055
Junlei <749101340@qq.com>
2025-08-14 15:15:45 +0800

    feat(用户管理-微信用户)
    - 添加"认养情况""状态"查询及列表展示

--- apps/web-antd/src/views/user/wx_user/data.ts
+++ apps/web-antd/src/views/user/wx_user/data.ts
@@ -18,15 +18,26 @@ export interface ITableData {
   extra_obj: null | string;
 }
 
-/** 表单配置 */
-export function useFormSchema(): VbenFormSchema[] {
-  return [
-    {
-      component: 'Input',
-      fieldName: 'nickname',
-      label: '名字',
+/** 账号状态 */
+const userState = [
+  { label: '游客', value: 1 },
+  { label: '普通用户', value: 2 },
+  { label: '代理商会员', value: 3 },
+];
+const userStateStr = generateObj(userState);
+
+/** 认养情况 */
+const adoptState = [{ label: '未认养', value: 1 }];
+const adoptStateStr = generateObj(adoptState);
+
+function generateObj(data: { label: string; value: number | string }[]) {
+  return data.reduce(
+    (pre, cur) => {
+      pre[cur.value] = cur.label;
+      return pre;
     },
-  ];
+    {} as Record<number | string, string>,
+  );
 }
 
 /** 列表搜索 */
@@ -49,6 +60,24 @@ export function useGridFormSchema(): VbenFormSchema[] {
       label: '手机号',
     },
     {
+      label: '状态',
+      fieldName: 'status',
+      component: 'Select',
+      componentProps: {
+        allowClear: true,
+        options: userState,
+      },
+    },
+    {
+      label: '认养情况',
+      component: 'Select',
+      fieldName: 'got_tree',
+      componentProps: {
+        allowClear: true,
+        options: adoptState,
+      },
+    },
+    {
       component: 'RangePicker',
       componentProps: DRangePickerProps,
       fieldName: 'created_at',
@@ -65,6 +94,7 @@ export function useColumns<T = any>(
     {
       field: 'nickname',
       title: '用户名',
+      fixed: 'left',
     },
     {
       title: 'ID',
@@ -75,6 +105,18 @@ export function useColumns<T = any>(
     {
       field: 'phone',
       title: '手机号',
+    },
+    {
+      title: '状态',
+      slots: {
+        default: ({ row }) => userStateStr[row.status] ?? '',
+      },
+    },
+    {
+      title: '认养情况',
+      slots: {
+        default: ({ row }) => adoptStateStr[row.got_tree] ?? '',
+      },
     },
     // {
     //   field: 'email',
